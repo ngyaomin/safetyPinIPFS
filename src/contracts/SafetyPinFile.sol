db5dcd7875770c1fe7262337d5b38b4dba70429d
Yao Min <ngyaominsg@gmail.com>
2020-12-22 13:57:10 +0800

    Set up smart contract

---
+++ src/contracts/SafetyPinFile.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.16;
+
+contract SafetyPinFile {
+  string ipfsHash;
+  function set(string memory _ipfsHash) public {
+    ipfsHash = _ipfsHash;
+  }
+
+  function get()public view returns (string memory) {
+    return ipfsHash;
+  }
+}
---
+++ test/SafetyPinFile.test.js
@@ -0,0 +1,11 @@
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('SafetyPinFile', (accouts) => {
+  let safetyPinFile
+
+  before(() => {
+    
+  })
+})
